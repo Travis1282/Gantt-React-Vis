e915e838a09fb9b071eeb01a4ccd37a0ebee26dc
Anthony <anthonyjlower@gmail.com>
2018-02-12 16:47:12 -0600

    Edited timeline options

--- src/ItemTimeline.js
+++ src/ItemTimeline.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import './App.js'
 import Timeline from 'react-visjs-timeline'
 
-const options = {
-  width: '100%',
-  height: '90vh',
-  stack: false,
-  showMajorLabels: true,
-  showCurrentTime: true,
-  zoomMin: 1000000,
-  zoomMax: 100000000000,
-  orientation: 'top',
-  selectable: true,
-  stack: true,
-  stackSubgroups: true,
-  // clickToUse: true,
-  groupEditable: true,
-  format: {
-    minorLabels: {
-      minute: 'h:mma',
-      hour: 'ha'
-    }
-  }
-}
+// const options = {
+//   width: '100%',
+//   height: '90vh',
+//   stack: false,
+//   showMajorLabels: true,
+//   showCurrentTime: true,
+//   zoomMin: 1000000,
+//   zoomMax: 100000000000,
+//   orientation: 'top',
+//   selectable: true,
+//   stack: true,
+//   stackSubgroups: true,
+//   editable: true,
+//   format: {
+//     minorLabels: {
+//       minute: 'h:mma',
+//       hour: 'ha'
+//     }
+//   },
+//   onUpdate(e) {
+//     console.log(e)
+//   }
+// }
+
+
 
 
   const getItemStyle = () => ({
@@ -38,15 +42,41 @@ class ItemTimeline extends Component {
   constructor(props){
     super(props)
     this.state = {
-      items: this.props.selectedProject
+      items: this.props.selectedProject,
+      editedItem: ""
+    }
+  }
+
+
+  options = {
+    width: '100%',
+    height: '90vh',
+    stack: false,
+    showMajorLabels: true,
+    showCurrentTime: true,
+    zoomMin: 1000000,
+    zoomMax: 100000000000,
+    orientation: 'top',
+    selectable: true,
+    stack: true,
+    stackSubgroups: true,
+    editable: true,
+    format: {
+      minorLabels: {
+        minute: 'h:mma',
+        hour: 'ha'
+      }
+    },
+    onUpdate: (e) => {
+      this.setState({editedItem: e})
+      console.log(this.state)
     }
   }
 
   render(){
-    console.log(this.state.items)
   return (
     <div>
-      <Timeline options={options} items={this.state.items}/>
+      <Timeline options={this.options} items={this.state.items}/>
       Timeline
     </div>
   );
